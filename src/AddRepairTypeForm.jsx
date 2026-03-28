53d1d31ba025ef9d5157e4012bb5c993f61a99ce
AndrewYatkin <andrew.yatkin@gmail.com>
2026-03-28 12:32:11 +0300

    AddRepairTypeForm fixed

--- src/AddRepairTypeForm.jsx
+++ src/AddRepairTypeForm.jsx
@@ -2,8 +2,10 @@ import React, {useState} from 'react';
 import {supabase} from './supabaseClient';
 
 function AddRepairTypeForm({onRepairTypeAdded}) {
-    const [repairTypeCode, setRepairTypeCode] = useState(0);
     const [repairCode, setRepairCode] = useState(0);
+    const [name, setName] = useState('');
+    const [duration, setDuration] = useState(0);
+    const [price, setPrice] = useState(0.0);
     const [description, setDescription] = useState('');
 
     const [loading, setLoading] = useState(false);
@@ -17,7 +19,12 @@ function AddRepairTypeForm({onRepairTypeAdded}) {
         try {
             const {data, error} = await supabase
                 .from('repair_type')
-                .insert([{repair_type_code: repairTypeCode, email: repairCode}]);
+                .insert([{
+                    repair_code: repairCode,
+                    name: name,
+                    duration: duration,
+                    price: price,
+                    description: description}]);
             if (error) {
                 throw error;
             }
@@ -25,8 +32,10 @@ function AddRepairTypeForm({onRepairTypeAdded}) {
             if (onRepairTypeAdded && data && data.length > 0) {
                 onRepairTypeAdded(data[0]);
             }
-            setRepairTypeCode(0);
             setRepairCode(0);
+            setName('');
+            setDuration(0);
+            setPrice(0.0);
             setDescription('')
             alert('Тип ремонта успешно добавлен');
         } catch (error) {
@@ -39,26 +48,48 @@ function AddRepairTypeForm({onRepairTypeAdded}) {
 
     return (
         <div>
-            <h2>Добавить новый ремонт</h2>
+            <h2>Добавить новый тип ремонта</h2>
             <form onSubmit={handleSubmit}>
                 <div>
-                    <label htmlFor="repairTypeCode">Код типа ремонта</label>
-                    <input
-                        type="number"
-                        id="repairTypeCode"
-                        value={repairTypeCode}
-                        onChange={(e) => setRepairTypeCode(e.target.value)}
-                        required
-                        disabled={loading}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="repairCode">Код ремонта</label>
+                    <label htmlFor="repairCode">Код типа ремонта</label>
                     <input
                         type="number"
                         id="repairCode"
                         value={repairCode}
                         onChange={(e) => setRepairCode(e.target.value)}
+                        required
+                        disabled={loading}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="name">Название ремонта</label>
+                    <input
+                        type="text"
+                        id="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        required
+                        disabled={loading}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="duration">Продолжительность ремонта</label>
+                    <input
+                        type="number"
+                        id="duration"
+                        value={duration}
+                        onChange={(e) => setDuration(e.target.value)}
+                        required
+                        disabled={loading}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="price">Стоимость ремонта</label>
+                    <input
+                        type="number"
+                        id="price"
+                        value={price}
+                        onChange={(e) => setPrice(e.target.value)}
                         required
                         disabled={loading}
                     />
@@ -75,7 +106,7 @@ function AddRepairTypeForm({onRepairTypeAdded}) {
                     />
                 </div>
                 <button type="submit" disabled={loading}>
-                    {loading ? 'Добавление...' : 'Добавить ремонт'}
+                    {loading ? 'Добавление...' : 'Добавить тип ремонта'}
                 </button>
                 {error && <p className="error"> L85:4: {error}</p>}
             </form>
